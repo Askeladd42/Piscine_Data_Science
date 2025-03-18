e03aa9d2bb69c3b57746a5b339ec9c3036f6f7cd
plam <plam@student.42.fr>
2025-03-18 16:37:57 +0100

    updated gitignore for future env tests

---
+++ Day00/ex02/table.sh
@@ -0,0 +1,15 @@
+# bash
+
+
+### table creation in the Postgres database
+CREATE TABLE data_2022_oct (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT,
+    user_id BIGINT,
+    user_session UUID
+)
+
+### Copy data from the CSV file to the table
+COPY data_2022_oct FROM 'data_2022_oct.csv' DELIMITER ',' CSV HEADER
