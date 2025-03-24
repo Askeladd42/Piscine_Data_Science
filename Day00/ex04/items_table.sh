470cb935866d19cb22707963b556acbdf40454db
plam <plam@student.42.fr>
2025-03-24 18:46:54 +0100

    Day00 finished

---
+++ Day00/ex04/items_table.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Directory containing the CSV file
+CSV_DIR="../../ressources/Piscine_DataScience/subject_D00/items"
+
+# Database connection details, to be deleted when using the .env later on
+DB_NAME="postgres_db"
+DB_USER="plam"
+DB_HOST="piscineds"
+DB_PORT="5432:5432"
+
+# CSV file path
+csv_file="$CSV_DIR/items.csv"
+
+# Table name
+table_name="items"
+
+# Create the table using the extracted name
+echo "Creating table: $table_name"
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "
+CREATE TABLE IF NOT EXISTS \"$table_name\" (
+    product_id INT,
+    category_id BIGINT,
+    category_code VARCHAR(255),
+    brand VARCHAR(255)
+);"
+
+# Copy data from the CSV file to the table
+echo "Copying data to table: $table_name"
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "
+COPY \"$table_name\" FROM '$csv_file' DELIMITER ',' CSV HEADER;"
