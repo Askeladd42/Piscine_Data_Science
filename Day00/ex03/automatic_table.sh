b2a402fb19e1b7565a838b4477097571b24d43cc
plam <plam@student.42.fr>
2025-03-25 19:56:20 +0100

    Day00 finished, updated to avoid error usages

--- Day00/ex03/automatic_table.sh
+++ Day00/ex03/automatic_table.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+# Load environment variables from test.env, to modify if necessary
+set -o allexport
+source ../../test.env
+set +o allexport
+
 # Directory containing the CSV files
 CSV_DIR="../../ressources/Piscine_DataScience/subject_D00/customer"
 
 # Database connection details, to be deleted when using the .env later on
-DB_NAME="postgres_db"
-DB_USER="plam"
-DB_HOST="piscineds"
-DB_PORT="5432:5432"
+# DB_NAME="postgres_db"
+# DB_USER="plam"
+# DB_HOST="piscineds"
+# DB_PORT="5432:5432"
 
 # Loop through all CSV files in the directory
 for csv_file in "$CSV_DIR"/*.csv; do
     # Extract the base name of the file (without the directory and extension)
     table_name=$(basename "$csv_file" .csv)
-    
+
     # Create the table using the extracted name
     echo "Creating table: $table_name"
-    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "
+    psql -h "$POSTGRES_DB" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_NAME" -c "
     CREATE TABLE IF NOT EXISTS \"$table_name\" (
         event_time TIMESTAMP,
         event_type VARCHAR(255),
@@ -28,6 +33,6 @@ for csv_file in "$CSV_DIR"/*.csv; do
     
     # Copy data from the CSV file to the table
     echo "Copying data to table: $table_name"
-    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "
+    psql -h "$POSTGRES_DB" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_NAME" -c "
     COPY \"$table_name\" FROM '$csv_file' DELIMITER ',' CSV HEADER;"
 done
