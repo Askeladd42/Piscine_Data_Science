5292060cbf12dfc0d9e80077b74b8a81a1c26934
plam <plam@student.42.fr>
2025-03-27 23:59:04 +0100

    Day00 finished, updated to avoid error usages, Day01 started

--- Day01/ex01/customers_table.sh
+++ Day01/ex01/customers_table.sh
@@ -6,10 +6,5 @@ source ../../test.env
 set +o allexport
 
 # Directory containing the CSV files
-CSV_DIR="../../ressources/Piscine_DataScience/subject_D01/customer"
+CSV_DIR="../../ressources/Piscine_DataScience/D01"
 
-# Database connection details, to be deleted when using the .env later on
-# DB_NAME="postgres_db"
-# DB_USER="plam"
-# DB_HOST="piscineds"
-# DB_PORT="5432"
