b96fd9ecd8beed6ca9a61b8ccccdba2e0d3e6816
Pierre-marie LAM <plam@f2r10s13.clusters.42paris.fr>
2025-06-04 13:55:51 +0200

    ajusting the fusion treatment for customers and items

--- Day01/ex03/fusion.sh
+++ Day01/ex03/fusion.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-# Load environment file
 set -o allexport
-source /home/plam/sgoinfre/test.env    # Adjust the path to your .env file
+source /home/plam/sgoinfre/test.env     # Adjust the path as necessary
 set +o allexport
 
-# Table name
 table_name="customers"
 
-# Fusion of the "customers" table with the "items" table in the "customers" table
-echo "Fusion of the $table_name table with the items table"
-docker exec -it "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
-UPDATE \"$table_name\"
-SET items = (
-    SELECT items
-    FROM items
-    WHERE items.id = \"$table_name\".items_id
-);
+# Ajoute les colonnes si elles n'existent pas déjà
+for col in "category_id BIGINT" "category_code VARCHAR(255)" "brand VARCHAR(255)"; do
+    name=$(echo $col | cut -d' ' -f1)
+    type=$(echo $col | cut -d' ' -f2-)
+    docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1 FROM information_schema.columns
+            WHERE table_name = '$table_name' AND column_name = '$name'
+        ) THEN
+            ALTER TABLE \"$table_name\" ADD COLUMN $name $type;
+        END IF;
+    END
+    \$\$;"
+done
+
+# Met à jour customers avec les infos de items (jointure sur product_id)
+docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
+UPDATE \"$table_name\" AS c
+SET
+    category_id = i.category_id,
+    category_code = i.category_code,
+    brand = i.brand
+FROM items i
+WHERE c.product_id = i.product_id;
+"
 
 echo "Fusion of the $table_name table with the items table completed"
