a3eff4a68797aca8ce3b9d127c132d38c3556926
Pierre-marie LAM <plam@f2r10s13.clusters.42paris.fr>
2025-06-04 13:35:48 +0200

    batch treatment for faster and better duplicate cleaning

--- Day01/ex02/remove_duplicates.sh
+++ Day01/ex02/remove_duplicates.sh
@@ -6,6 +6,7 @@ source /home/plam/sgoinfre/test.env     # Adjust the path as necessary
 set +o allexport
 
 table_name="customers"
+batch_size=10000    # batch treatment size because of the size of the table
 
 # Get columns except event_time
 columns=$(docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -A -c \
@@ -20,19 +21,32 @@ fi
 # ctid is used to uniquely identify rows in PostgreSQL
 
 echo "Removing near-duplicate rows from table: $table_name"
-docker exec -i "$DB_CONTAINER" psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
-DELETE FROM \"$table_name\"
-WHERE ctid IN (
-    SELECT ctid FROM (
-        SELECT
-            ctid,
-            ROW_NUMBER() OVER (
-                PARTITION BY $columns, date_trunc('second', event_time)
-                ORDER BY event_time
-            ) AS rn
-        FROM \"$table_name\"
-    ) t
-    WHERE t.rn > 1
-);"
+while :; do
+    deleted=$(docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -A -c "
+        WITH to_delete AS (
+            SELECT ctid
+            FROM (
+                SELECT
+                    ctid,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY $columns, date_trunc('second', event_time)
+                        ORDER BY event_time
+                    ) AS rn
+                FROM \"$table_name\"
+            ) t
+            WHERE t.rn > 1
+            LIMIT $batch_size
+        )
+        DELETE FROM \"$table_name\" WHERE ctid IN (SELECT ctid FROM to_delete)
+        RETURNING 1;
+    " | wc -l)
 
-echo "Near-duplicate rows removed from table: $table_name"
+    if [ "$deleted" -eq 0 ]; then
+        echo "No more duplicates to delete."
+        break
+    else
+        echo "Deleted $deleted duplicate rows in this batch..."
+    fi
+done
+
+echo "All near-duplicate rows removed from table: $table_name"
