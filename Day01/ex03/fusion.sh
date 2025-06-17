ad9ce8b22405d5a8e99aec943abf78be60872e72
Pierre-marie LAM <plam@f6r9s1.clusters.42paris.fr>
2025-06-17 14:44:53 +0200

    ajusting the fusion treatment for customers and items

--- Day01/ex03/fusion.sh
+++ Day01/ex03/fusion.sh
@@ -26,30 +26,52 @@ for col in "category_id BIGINT" "category_code VARCHAR(255)" "brand VARCHAR(255)
     \$\$;"
 done
 
-echo "Vérification des doublons de product_id dans la table items..."
+echo "Verifying if the rows in the $table_name table are unique based on product_id"
 duplicates=$(docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -A -c "
 SELECT product_id FROM items GROUP BY product_id HAVING COUNT(*) > 1;
 ")
 
 if [ -n "$duplicates" ]; then
-    echo "ATTENTION : Il existe des doublons de product_id dans la table items :"
+    echo "WARNING: duplicates product_id found in the items table:"
     echo "$duplicates"
-    echo "Arrêt du script pour éviter une fusion incorrecte."
-    exit 1
 else
     echo "Aucun doublon de product_id trouvé dans la table items."
 fi
 
-# Update the customers table with data from the items table
-echo "Updating the $table_name table with data from the items table"
+echo "Verifying if the duplicates in the items table have different values for category_id, category_code, and brand"
+diff_duplicates=$(docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -A -c "
+SELECT product_id
+FROM items
+GROUP BY product_id
+HAVING COUNT(*) > 1
+   AND COUNT(DISTINCT (category_id, category_code, brand)) > 1;
+")
+
+if [ -n "$diff_duplicates" ]; then
+    echo "ATTENTION : Certains product_id ont des valeurs différentes dans items !"
+    echo "$diff_duplicates"
+else
+    echo "Tous les doublons de product_id sont identiques sur toutes les colonnes."
+fi
+
+echo "Creating a temporary table items_nodup to remove duplicates based on product_id and keep the first occurrence"
+docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
+DROP TABLE IF EXISTS items_nodup;
+CREATE TEMP TABLE items_nodup AS
+SELECT DISTINCT ON (product_id) *
+FROM items
+ORDER BY product_id;
+"
+
+# Update the customers table with data from the items_nodup table
+echo "Updating the $table_name table with data from the items table (sans doublons)"
 docker exec -i "$DB_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
 UPDATE \"$table_name\" AS c
 SET
     category_id = i.category_id,
     category_code = i.category_code,
     brand = i.brand
-FROM items i
+FROM items_nodup i
 WHERE c.product_id = i.product_id;
 "
-
 echo "Fusion of the $table_name table with the items table completed"
